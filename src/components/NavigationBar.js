1c18ac12bb892d42c847783faa9db5da4e11d559
Edward <edwardlinton35@gmail.com>
2022-01-11 21:48:40 +0000

    wip: temporary changes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -45,7 +45,7 @@ class NavigationBar extends React.Component{
 					<Nav>
 						<Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
 						<Nav.Item><Nav.Link href="/gallery">Gallery</Nav.Link></Nav.Item>
-						<Nav.Item><Nav.Link href="/shop">Shop</Nav.Link></Nav.Item>
+						<Nav.Item><Nav.Link href="/shop">Gifts</Nav.Link></Nav.Item>
 						<Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
 						<Nav.Item><Nav.Link href="https://stardustimagesblog.wordpress.com/">Blog</Nav.Link></Nav.Item>
 					</Nav>
